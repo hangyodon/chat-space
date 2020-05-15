bb135aec00e083411c759d8e56982a2ae4a4e5a3
hangyodon <64734488+hangyodon@users.noreply.github.com>
2020-05-15 19:02:37 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $ (function (){
+
   function buildHtml(message){
     if (message.image) {
       var html =
-      `<div class="main-chat__messages__message">
+      `<div class="main-chat__messages__message" data-message-id=${message.id}>
         <div class="main-chat__messages__message__upper-info">
           <div class="main-chat__messages__message__upper-info__talker">
             ${message.user_name}
@@ -21,7 +22,7 @@ $ (function (){
       return html;
     } else {
       var html =
-      `<div class="main-chat__messages__message">
+      `<div class="main-chat__messages__message" data-message-id=${message.id}>
         <div class="main-chat__messages__message__upper-info">
           <div class="main-chat__messages__message__upper-info__talker">
             ${message.user_name}
@@ -63,4 +64,30 @@ $ (function (){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.main-chat__messages__message:last').data("message-id");
+    $.ajax({
+      url: 'api/messages',
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i,message) {
+        insertHTML += buildHtml(message)
+      });
+      $('.main-chat__messages').append(insertHTML);
+      $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
